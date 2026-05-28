43a82765e3c25558b37883bdcb1ee2ebead4518f
kunil-choi <gaegoo99@gmail.com>
2026-05-28 15:04:30 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,8 @@
 // ── API 및 팀 설정 ──
-// Google Apps Script 웹앱 URL: 변경 시 이 파일만 수정
-const API = 'https://script.google.com/macros/s/AKfycbyczWT77wQ1GcpPnTrwUybyau8zDjuKAZMw0XUC3Cm8CRKDSlJHgQW4pDGycbguQCjk/exec';
-
 const PRODUCERS = ['', '양영은', '최건일'];
 const EDITORS   = ['', '박채빈', '김태환', '외주'];
 const TIMES     = ['','09:00','10:00','10:30','11:00','11:30','12:30',
                    '13:00','14:00','15:00','15:30','16:00','17:00','이어서','-'];
-
 const DAY_KO = ['일','월','화','수','목','금','토'];
 
 function toKey(d){
